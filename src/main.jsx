24907751d614b1960e10e9b86d8e5091f3dffa4a
priyanshu007kyadav-lab <priyanshu007kyadav@gmail.com>
2026-09-02 22:40:28 +0530

    posthog

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,28 @@ import App from "./App.jsx";
 import { StoryProvider } from "./story/StoryContext";
 import "./styles/intro.css";
 import { CameraProvider } from "./story/CameraContext";
+import { PostHogProvider } from "@posthog/react";
+
+const posthogApiKey =
+  import.meta.env.VITE_POSTHOG_PROJECT_TOKEN ||
+  "phc_pLTos9ws4shUwkmC9TqooGxwyBikLNfy7SBZhAnSdjvt";
+
+const posthogOptions = {
+  api_host:
+    import.meta.env.VITE_POSTHOG_HOST || "https://us.i.posthog.com",
+  person_profiles: "identified_only",
+  capture_pageview: true,
+  autocapture: true,
+};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <StoryProvider>
-
-     <CameraProvider>
-
-        <App />
-
-     </CameraProvider>
-
-    </StoryProvider>
+    <PostHogProvider apiKey={posthogApiKey} options={posthogOptions}>
+      <StoryProvider>
+        <CameraProvider>
+          <App />
+        </CameraProvider>
+      </StoryProvider>
+    </PostHogProvider>
   </StrictMode>
 );
